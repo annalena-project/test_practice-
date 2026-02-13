9e455d87219f517f9f0fb5cd8aed8e515b9c54fa
annalena-project <aschaeffer@northpark.edu>
2026-02-12 18:07:41 -0600

    added .venv i .gitignore

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# a.
+
 import requests 
 
 URL = "https://geocoding-api.open-meteo.com/v1/search"
@@ -13,15 +15,23 @@ response = requests.get(URL, params=parameters, timeout=10)
 # params=parameters: The server uses them to know what to return.
 
 print(response.status_code)   # Prints the HTTP status code. It shows what response the server sent back (200, 400, 500)
-print(response.json())        # Converts the server response to a Python dictionary and prints the data
+#print(response.json())        # Converts the server response to a Python dictionary and prints the data
 
 data = response.json()        # Converts the server response from JSON format to a Python dictionary
 
 results = data["results"]     
+# Works with the information that data received from the URL.
+# Goes into the dictionary "data" → finds the key "results" → 
+# gets the value stored there → saves it in the variable "results".
 first_result = results[0]
+# Takes the first item from the results list and stores it in the variable first_result
 
-latitude = first_result["latitude"]   
+latitude = first_result["latitude"]   # Gets the value of "latitude" / "longitude" from the first_result dictionary
 longitude = first_result["longitude"]
 
 print("latitude:", latitude)   # Prints the latitude / longitude value
 print("longitude:", longitude)
+
+
+# b. 
+
