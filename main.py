705b40fae56e5903ce795d3383d09a38ebd0424d
annalena-project <aschaeffer@northpark.edu>
2026-02-20 09:00:56 -0600

    added json dum etc

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # a.
 
 import requests 
+import json
 
 URL = "https://geocoding-api.open-meteo.com/v1/search"
 
 parameters = {
-    "name": "Karlstad",         # city 
-    "country": "Sweden",
+    "name": "Chicago",         # city 
+    "country": "USA",
     "count": 1
 }
 
@@ -34,12 +35,11 @@ print("longitude:", longitude)
   
 # b. 
 
-FORECAST_URL = "https://api.open-meteo.com/v1/forecast"
+FORECAST_URL = "https://api.open-meteo.com/v1/forecast?current_weather=true"
 
 forecast_parameters = {
     "latitude": latitude,
     "longitude": longitude,
-    "current_weather": True
 }
 
 forecast_response = requests.get(FORECAST_URL, params=forecast_parameters, timeout=10)
@@ -47,8 +47,39 @@ forecast_response = requests.get(FORECAST_URL, params=forecast_parameters, timeo
 forecast_data = forecast_response.json()
 print(forecast_response.status_code)
 
+print(json.dumps(forecast_data, indent=2))
+
 # C
 
-print(forecast_data)
+class WeatherReport:
+    def __init__(self, city, country, latitude, longitude, temperature, elevation, windspeed, observation_time):
+        self.city = city
+        self.country = country
+        self.latitude = latitude
+        self.longitude = longitude
+        self.temperature = temperature
+        self.elevation = elevation
+        self.windspeed = windspeed
+        self.observation_time = observation_time
+
+report = WeatherReport(
+    city=parameters["name"],
+    country=parameters["country"],
+    latitude=forecast_parameters["latitude"],
+    longitude=forecast_parameters["longitude"],
+    temperature=forecast_data["current_weather"]["temperature"],
+    elevation=forecast_data["elevation"],
+    windspeed=forecast_data["current_weather"]["windspeed"],
+    observation_time=forecast_data["current_weather"]["time"]
+)
+
+print("city:", report.city)
+print("country:", report.country)
+print("latitude:", report.latitude)
+print("longitude:", report.longitude)
+print("temperature:", report.temperature)
+print("elevation:", report.elevation)
+print("windspeed:", report.windspeed)
+print("time:", report.observation_time)
 
 
